275a0f7ba917b5cd1b8e21105b8c2cbf3cf06148
Ulysse Gérard <thevoodoos@gmail.com>
2021-02-24 15:53:40 +0100

    Static for gh pages

---
+++ dist/js/learnocaml-config.js
@@ -0,0 +1,8 @@
+var learnocaml_config = {
+  enableTryocaml: false,
+  enablePlayground: true,
+  enableLessons: false,
+  enableExercises: false,
+  enableToplevel: true,
+  baseUrl: "https://voodoos.github.io/learn-ocaml"
+}
